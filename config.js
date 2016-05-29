59ffa2b3dbde14e3ae53e7823e78c5f036dc184e
Hadi Farnoud <hadi.farnoud@gmail.com>
2016-05-30 00:12:10 +0430

    fix SMTP server

--- config.js
+++ config.js
@@ -44,7 +44,6 @@ config = {
       options: {
         service: 'Sendinblue',
         host: process.env.SMTP_SERVER.
-        port: process.env.SMTP_PORT,
         auth: {
             user: process.env.SMTP_USERNAME,
             pass: process.env.SMTP_PASSWORD,
